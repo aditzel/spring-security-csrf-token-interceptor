a4b015befda2a7cf544254ff8819f9179f4c0928
Allan Ditzel <allan@allanditzel.com>
2014-07-05 11:14:57 -0500

    Adding solution to address issue #2. Interceptor will now do another HEAD request to retrieve the CSRF token if the response is 403.

--- src/spring-security-csrf-token-interceptor.js
+++ src/spring-security-csrf-token-interceptor.js
@@ -19,29 +19,36 @@
  *
  * Sets up an interceptor for all HTTP requests that adds the CSRF Token Header that Spring Security requires.
  */
-
-angular.module('spring-security-csrf-token-interceptor', [])
-    .config(function($httpProvider) {
-        var getTokenData = function() {
-            var defaultCsrfTokenHeader = 'X-CSRF-TOKEN';
-            var csrfTokenHeaderName = 'X-CSRF-HEADER';
-            var xhr = new XMLHttpRequest();
-            xhr.open('head', '/', false);
-            xhr.send();
-            var csrfTokenHeader = xhr.getResponseHeader(csrfTokenHeaderName);
-            csrfTokenHeader = csrfTokenHeader ? csrfTokenHeader : defaultCsrfTokenHeader;
-            return { headerName: csrfTokenHeader, token: xhr.getResponseHeader(csrfTokenHeader) };
-        };
-        var csrfTokenData = getTokenData();
-        $httpProvider.interceptors.push(function($q) {
-            return {
-                request: function(config) {
-                    if (config.method.toLowerCase() == "post") {
-                        csrfTokenData = getTokenData();
-                    }
-                    config.headers[csrfTokenData.headerName] = csrfTokenData.token;
-                    return config || $q.when(config);
-                }
+(function () {
+    angular.module('spring-security-csrf-token-interceptor', [])
+        .config(function ($httpProvider) {
+            var getTokenData = function () {
+                var defaultCsrfTokenHeader = 'X-CSRF-TOKEN';
+                var csrfTokenHeaderName = 'X-CSRF-HEADER';
+                var xhr = new XMLHttpRequest();
+                xhr.open('head', '/', false);
+                xhr.send();
+                var csrfTokenHeader = xhr.getResponseHeader(csrfTokenHeaderName);
+                csrfTokenHeader = csrfTokenHeader ? csrfTokenHeader : defaultCsrfTokenHeader;
+                return { headerName: csrfTokenHeader, token: xhr.getResponseHeader(csrfTokenHeader) };
             };
+            var csrfTokenData = getTokenData();
+            $httpProvider.interceptors.push(function ($q, $injector) {
+                return {
+                    request: function (config) {
+                        config.headers[csrfTokenData.headerName] = csrfTokenData.token;
+                        return config || $q.when(config);
+                    },
+                    responseError: function (response) {
+                        console.log("There was an error processing the request", response);
+                        if (response.status == 403) {
+                            csrfTokenData = getTokenData();
+                            var $http = $injector.get('$http');
+                            return $http(response.config);
+                        }
+                        return response;
+                    }
+                };
+            });
         });
-    });
+})();
